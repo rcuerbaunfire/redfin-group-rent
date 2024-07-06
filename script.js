a63184ba230f06ace4715fc8a8ded6cebd9c6e7a
Rich Philben Cuer <shinigami.richi@gmail.com>
2024-07-06 08:22:59 +0800

    updates to layout

--- script.js
+++ script.js
@@ -869,7 +869,7 @@ $(document).ready(function () {
             const self = $(this);
             const heroRight = self.find(".hero-right-container");
             const heroLeft = self.find(".hero-left-container");
-            const heroLeftGroup1 = self.find(".richtext-heading");
+            const heroLeftGroup1 = self.find(".richtext-heading, .text-block.is-hero");
             const heroLeftGroup2 = self.find(".get-started-slider");
 
             gsap.timeline()
